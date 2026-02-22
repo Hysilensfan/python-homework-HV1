051d8550a3d03a56a7cd6ff989c285419ac4a207
Hysilensfan <uraume2027@gmail.com>
2026-02-23 00:09:22 +0800

    Update py 014.py

--- py 014.py
+++ py 014.py
@@ -3,17 +3,14 @@ for i in range(1,n+1): #control number of rows
     for u in range(1,i+1): #controlling internal loop and print 1 to i
         print(u,end=' ') #print digits and don't turn to newline
     print() #newline
-print()
 for i in range(1,n+1):
     for u in range(1,n-i+2): #each row prints one less number than the previous
         print(u,end=' ')
     print()
-print()
 for i in range(1,n+1):
     for u in range(n,i-1,-1): # print numbers from n down to i
         print(u,end=' ')
     print()
-print()
 for i in range(1,n+1):
     for j in range(n-i): # repeat n-i times
         print(' ',end=' ') #printing spaces for right alignment
