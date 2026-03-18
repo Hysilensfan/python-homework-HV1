107fe9e6c73491f8332fefc2484653ec4ced3ab5
Hysilensfan <uraume2027@gmail.com>
2026-03-18 08:31:54 +0800

    Create py 032.py

---
+++ py 032.py
@@ -0,0 +1,8 @@
+# first method
+for _ in range(int(input())):
+    a = input()
+    if (sum(map(int,str(int(a[0])*2))) + sum(map(int,str(int(a[1])*1))) + sum(map(int,str(int(a[2])*2))) + sum(map(int,str(int(a[3])*1))) + sum(map(int,str(int(a[4])*2))) + sum(map(int,str(int(a[5])*1))) + sum(map(int,str(int(a[6])*2))) + sum(map(int,str(int(a[7])*1))) + sum(map(int,str(int(a[8])*2))) + sum(map(int,str(int(a[9])*1))) + sum(map(int,str(int(a[10])*2))) + sum(map(int,str(int(a[11])*1))) + sum(map(int,str(int(a[12])*2))) + sum(map(int,str(int(a[13])*1))) + sum(map(int,str(int(a[14])*2))) + sum(map(int,str(int(a[15])*1))))%10==0:
+        print("T")
+    else:
+        print("F")
+# second method
