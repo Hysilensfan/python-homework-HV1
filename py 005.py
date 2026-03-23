99625ecfc92af74061185df9ff701371a17cf173
Hysilensfan <uraume2027@gmail.com>
2026-03-23 09:27:19 +0800

    Update py 005.py

--- py 005.py
+++ py 005.py
@@ -1,2 +1,1 @@
-s = int(input())
-print(f"{s//(24*60**2)}D{s%(24*60**2)//60**2}H{s%(24*60**2)%(60**2)//60}M{s%(24*60**2)%(60**2)%60}S") #hint:1day24hour;1hour60minute;1minute60second;1day=24x60x60=86400second
+s = int(input());print(f"{s//(24*60**2)}D{s%(24*60**2)//60**2}H{s%(24*60**2)%(60**2)//60}M{s%(24*60**2)%(60**2)%60}S") #hint:1day24hour;1hour60minute;1minute60second;1day=24x60x60=86400second
