e97b97d3994103715b050580817f29b95678e952
Hysilensfan <uraume2027@gmail.com>
2026-04-19 00:30:35 +0800

    Update py 034.py

--- py 034.py
+++ py 034.py
@@ -1,8 +1,3 @@
-# first method
-for _ in range(int(input())):
-    a=input().replace(" ","");b=input().replace(" ","")
-    print("0" if set(a[1:]).isdisjoint(b[1:]) else len(set(b[1:]).intersection(a[1:])))
-# second method
 for _ in range(int(input())):
     a = list(map(int,input().split(' ')));b = list(map(int,input().split(' ')))
     print(len(set(a[1:]).intersection(b[1:])))
