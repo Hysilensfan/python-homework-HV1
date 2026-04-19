b2cc72862a5383d0182326a13c953a04c68bf744
Hysilensfan <uraume2027@gmail.com>
2026-04-20 01:15:50 +0800

    Update py 038.py

--- py 038.py
+++ py 038.py
@@ -1,1 +1,6 @@
-
+for _ in range(int(input())):
+    N = input();t = 4;A=B=''
+    for i in range(len(N)):
+        if N[i] == '4':A+='3';B+='1'
+        else:A+=N[i];B+='0'
+    print(f"{A} {int(B)}")
