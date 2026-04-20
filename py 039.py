c81387bb48ff45257d175a7fd128bec8b2fbaf19
Hysilensfan <uraume2027@gmail.com>
2026-04-21 00:27:38 +0800

    Update py 039.py

--- py 039.py
+++ py 039.py
@@ -1,1 +1,6 @@
-
+for _ in range(int(input())):
+    N = int(input());lk=[]
+    if N == 1:print(1);continue
+    for i in range (9,1,-1):
+        while N % i == 0:N//=i;lk.append(str(i))
+    print(-1 if N != 1 else"".join(sorted(lk)))
