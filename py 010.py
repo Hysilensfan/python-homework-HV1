94400388f9314fd12660669141958c8ec8074c67
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 21:40:52 +0800

    Update py 010.py

--- py 010.py
+++ py 010.py
@@ -5,7 +5,11 @@ if v == 0:
 elif v > 0:
     ans = f"{(-b-v**0.5)/(2*a)} {(-b+v**0.5)/(2*a)}"
 print(ans)
-#second method
-from math import sqrt        #"sqrt" calculates the real square root (with decimals);"isqrt" calculates only the integer part, directly discarding the decimals!
-def f(a,b,c):return b**2-a*c*4
-a , b , c = map(int, input().split());v = f(a,b,c);print(f"DR={b/(-2*a)}" if v == 0 else f"{(-b-sqrt(v))/(2*a)} {(-b+sqrt(v))/(2*a)}" if v > 0 else"NoSolution"))
+# second method
+from math import sqrt  # "sqrt" calculates the real square root (with decimals);"isqrt" calculates only the integer part, directly discarding the decimals!
+
+f = lambda a,b,c:b ** 2 - a * c * 4
+
+a, b, c = map(int, input().split())
+v = f(a, b, c)
+print(f"DR={b / (-2 * a)}" if v == 0 else f"{(-b - sqrt(v)) / (2 * a)} {(-b + sqrt(v)) / (2 * a)}" if v > 0 else "NoSolution")
