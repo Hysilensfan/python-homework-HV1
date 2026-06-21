fba11042066f94082d91758b8d369b01379eb3b4
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 20:38:58 +0800

    Update py 036.py

--- py 036.py
+++ py 036.py
@@ -1,1 +1,7 @@
-
+f = lambda s:s>1 and all(s % i != 0 for i in range(2, int(s**0.5)+1))
+def k(y):
+    if f(y-1):return y-1
+    else:return k(y-1)
+for i in range(int(input())):
+    print(k(int(input())))
+  
