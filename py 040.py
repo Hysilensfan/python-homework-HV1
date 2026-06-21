45f80e71b65f0f589668da0724f9e9e60cfb45ec
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 20:45:21 +0800

    Update py 040.py

--- py 040.py
+++ py 040.py
@@ -1,1 +1,9 @@
-
+for e in range(int(input())):
+    n = input();tot = 0;count_O=0
+    for c in n:
+        if c == "O":
+            tot = tot + 1 + count_O
+            count_O += 1
+        else:
+            count_O = 0
+    print(tot)
