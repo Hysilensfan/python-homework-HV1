3b94132b231a55dc0270f3370bafa33cbf3df6f9
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 21:34:03 +0800

    Update py 003.py

--- py 003.py
+++ py 003.py
@@ -1,11 +1,2 @@
-#first method
-y=int(input())
-ans="Wrong"
-if y>2:
-    ans=(y-2)*4+21
-elif 0<y<=2:
-    ans=y*10.5
-print(ans)
-#second method
 y = int(input())
 print((y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong")
