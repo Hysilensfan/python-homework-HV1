1bb314729496c18ed4a185cc9ff3367eb33d2c17
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 21:37:15 +0800

    Update py 008.py

--- py 008.py
+++ py 008.py
@@ -1,1 +1,2 @@
-s = input();print(f"{(s[:-3])}"if (s[0] in "aeiou" or s[1] in "aeiou") and s[-3:] == "way" else f"{(s[-3])+(s[:-3])}")
+s = input()
+print(f"{(s[:-3])}"if (s[0] in "aeiou" or s[1] in "aeiou") and s[-3:] == "way" else f"{(s[-3])+(s[:-3])}")
