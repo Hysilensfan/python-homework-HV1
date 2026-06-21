10f393da9988d67d9da76904fa6e4544123235b7
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 21:36:56 +0800

    Fix indentation in py 006.py

--- py 006.py
+++ py 006.py
@@ -1,1 +1,2 @@
-s = input();print(s if s == s[::-1] else f"{s[::-1]}{s}") # semicolon separates statements
+s = input()
+print(s if s == s[::-1] else f"{s[::-1]}{s}") # semicolon separates statements
