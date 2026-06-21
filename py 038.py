d59ba46d518387583e3f269f07140b1eb3c8b2d6
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 20:43:55 +0800

    Update py 038.py

--- py 038.py
+++ py 038.py
@@ -1,6 +1,11 @@
 for _ in range(int(input())):
-    N = input();t = 4;A=B=''
+    N, t = input(), 4
+    A = B = ""
     for i in range(len(N)):
-        if N[i] == '4':A+='3';B+='1'
-        else:A+=N[i];B+='0'
+        if N[i] == "4":
+            A += "3"
+            B += "1"
+        else:
+            A += N[i]
+            B += "0"
     print(f"{A} {int(B)}")
