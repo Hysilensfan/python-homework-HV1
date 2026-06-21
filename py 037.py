dc1ba106ba356dcd4ee44fbddf49f9ff1b65f9ea
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 20:42:29 +0800

    Update py 037.py

--- py 037.py
+++ py 037.py
@@ -1,5 +1,7 @@
 for _ in range(int(input())):
     a = input().split(" ")
     if len(a) < 22:
-        for c in range(len(a)):a[c]= int(a[c])
-        a = sorted(list(a));print(f"{int(a[-1])} {int(a[-2])}")
+        for c in range(len(a)):
+            a[c] = int(a[c])
+        a = sorted(list(a))
+        print(f"{int(a[-1])} {int(a[-2])}")
