9d0fa7d9decac889b1a70b17bdbff598b65f24c6
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 21:32:59 +0800

    Update py 003.py

--- py 003.py
+++ py 003.py
@@ -7,6 +7,5 @@ elif 0<y<=2:
     ans=y*10.5
 print(ans)
 #second method
-y = int(input());print((y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong")
-#third method
-y = int(input());r = lambda y: (y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong";print(r(y))
+y = int(input())
+print((y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong")
