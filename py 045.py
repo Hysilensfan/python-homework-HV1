6b8cab4d4cf6b9b9628b9d182c41969ac76f9b40
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 20:47:05 +0800

    Update py 045.py

--- py 045.py
+++ py 045.py
@@ -1,1 +1,8 @@
-
+for _ in range(int(input())):
+    b,c = input().split(', ');A=B=0
+    for e in range(len(b)):
+        if b[e] == c[e]:
+            A+=1
+        elif b[e] in c:
+            B+=1
+    print(f"{A}A{B}B")
