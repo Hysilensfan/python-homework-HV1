91c7e973c70c597eef57a77c09edbaf5d59f10a9
霧流羽kiriha <uraume2027@gmail.com>
2026-06-21 21:36:20 +0800

    Update py 005.py

--- py 005.py
+++ py 005.py
@@ -1,1 +1,8 @@
-s = int(input());print(f"{s//(24*60**2)}D{s%(24*60**2)//60**2}H{s%(24*60**2)%(60**2)//60}M{s%(24*60**2)%(60**2)%60}S") #hint:1day24hour;1hour60minute;1minute60second;1day=24x60x60=86400second
+s = int(input())
+print(f"{s//(24*60**2)}D{s%(24*60**2)//60**2}H{s%(24*60**2)%(60**2)//60}M{s%(24*60**2)%(60**2)%60}S")
+"""
+1day=24hour
+1hour=60minute
+1minute=60second
+1day=24hourx60minutex60second=86400second
+"""
