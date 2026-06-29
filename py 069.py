de618dffd0fcc228e69663dc4c913fbf2372369b
霧流羽kiriha <uraume2027@gmail.com>
2026-06-30 02:46:56 +0800

    Update py 069.py

--- py 069.py
+++ py 069.py
@@ -1,1 +1,18 @@
-
+def operation(g):
+    stack = []
+    for token in input().split():
+        if token.isdigit():
+            stack.append(int(token))
+        else:
+            b, a = stack.pop(), stack.pop()
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(a // b)
+    return stack[0]
+for _ in range(int(input())):
+    print(operation(input()))
